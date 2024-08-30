496c6d523d881a31fcaf5caa3d18b69f33295b78
James Stine <james.stine@okstate.edu>
2024-08-30 10:47:21 -0500

    update Lab2/3

---
+++ Lab3/Vivado/Lab1/Lab1.srcs/sources_1/imports/Desktop/top_demo.sv
@@ -0,0 +1,77 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/15/2021 06:40:11 PM
+// Design Name: 
+// Module Name: top_demo
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_demo
+(
+  // input
+  input  logic [7:0] sw,
+  input  logic [3:0] btn,
+  input  logic       sysclk_125mhz,
+  input  logic       rst,
+  // output  
+  output logic [7:0] led,
+  output logic sseg_ca,
+  output logic sseg_cb,
+  output logic sseg_cc,
+  output logic sseg_cd,
+  output logic sseg_ce,
+  output logic sseg_cf,
+  output logic sseg_cg,
+  output logic sseg_dp,
+  output logic [3:0] sseg_an
+);
+
+  logic [16:0] CURRENT_COUNT;
+  logic [16:0] NEXT_COUNT;
+  logic        smol_clk;
+  
+  // Place TicTacToe instantiation here
+  
+  // 7-segment display
+  segment_driver driver(
+  .clk(smol_clk),
+  .rst(btn[3]),
+  .digit0(sw[3:0]),
+  .digit1(4'b0111),
+  .digit2(sw[7:4]),
+  .digit3(4'b1111),
+  .decimals({1'b0, btn[2:0]}),
+  .segment_cathodes({sseg_dp, sseg_cg, sseg_cf, sseg_ce, sseg_cd, sseg_cc, sseg_cb, sseg_ca}),
+  .digit_anodes(sseg_an)
+  );
+
+// Register logic storing clock counts
+  always@(posedge sysclk_125mhz)
+  begin
+    if(btn[3])
+      CURRENT_COUNT = 17'h00000;
+    else
+      CURRENT_COUNT = NEXT_COUNT;
+  end
+  
+  // Increment logic
+  assign NEXT_COUNT = CURRENT_COUNT == 17'd100000 ? 17'h00000 : CURRENT_COUNT + 1;
+
+  // Creation of smaller clock signal from counters
+  assign smol_clk = CURRENT_COUNT == 17'd100000 ? 1'b1 : 1'b0;
+
+endmodule
